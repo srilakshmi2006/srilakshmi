578872190cbabc1a6ee102c152499b642f55a9e5
srilakshmi2006 <161099863+srilakshmi2006@users.noreply.github.com>
2024-02-24 08:57:01 +0530

    Add files via upload

---
+++ equal number or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+ {
+	int num1, num2;
+
+	num1=5;
+	num2=5;
+
+	if (num1==num2)
+	{
+		printf ("the numbers %d and %d are equal\n", num1,num2);
+	} else {
+		printf("the numbers %d and %d are not equal\n",num1,num2);
+	}
+	return 0;
+}
